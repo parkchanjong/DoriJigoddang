0786fd487e273bd80678c5747e91b3c74c0321f5
parkchanjong <7000cj@gmail.com>
2018-12-13 14:34:29 +0900

    dog

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -2,30 +2,22 @@ import Web3 from "web3";
 
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
-      // Modern dapp browsers...
       if (window.ethereum) {
         //사용자 승인을 거쳐 dapp 메타마스크
         const web3 = new Web3(window.ethereum);
         try {
-          // Request account access if needed
           await window.ethereum.enable(); //사용자 승인
-          // Acccounts now exposed
+
           resolve(web3);
         } catch (error) {
           reject(error);
         }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
+      } else if (window.web3) {
         const web3 = window.web3;
         console.log("Injected web3 detected.");
         resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
+      } else {
         const provider = new Web3.providers.HttpProvider(
           "http://127.0.0.1:9545"
         );
--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 module.exports = {
   networks: {
-    development: { host: "127.0.0.1", port: 7545, network_id: "5777" },
-    local_geth: { host: "127.0.0.1", port: 8545, network_id: "44" }
+    development: { host: "127.0.0.1", port: 8545, network_id: "5777" },
+    local_geth: { host: "127.0.0.1", port: 7545, network_id: "44" }
   }
 };
